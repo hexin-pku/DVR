update with test of HO case XXX fixed const.90 precision problem

ploteigv.py
@@ -6,12 +6,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 a=pd.read_csv('eigv.dat',header=None,sep= '\s+').values
-a=a.T
 for i in range(5):
     lenrange = len(a[i])
     x = np.linspace(-10,10,lenrange)
     ps = a[i]
-    plt.plot(x,ps,'-',label='%d'%i)
-
+    plt.plot(x,ps,'-',label='state %d-th'%i)
+plt.legend(loc=1)
 plt.show()
 

process.py
@@ -9,21 +9,24 @@ x,p = np.mgrid[-10:10:201j,-10:10:201j] # generate with the boundary
 x = x[1:-1,1:-1]
 p = p[1:-1,1:-1]
 
-rho = pd.read_csv('rho.dat',header=None,sep='\s+').values
+rho = pd.read_csv('rho.dat',header=None, sep='\s+').values
+
 poly = np.polynomial.laguerre.Laguerre((1))
 rhoexact = np.zeros((199,199))
+
 for i in range(len(x)):
     for j in range(len(p)):
-        xi = x[i,0]**2 + p[j,0]**2
+        xi = x[i,0]**2 + p[0,j]**2
         # (-1)^n (1/\[Pi]) E^-(p^2 + q^2) LaguerreL[n, 2 (p^2 + q^2)]
-        rhoexact[i,j] = np.exp(-xi) / np.pi
-         
+        rhoexact[i,j] = 1/np.pi * poly(2*xi) * np.exp(-xi)# L_n(x)
+        
 #print(len(rhoexact[0]))
 
 ax = plt.subplot(111, projection='3d')
 ax.set_title('denisty');
-ax.plot_surface(x,p,rhoexact,rstride=2, cstride=1, cmap=cm.jet)
-# ax.plot_surface(x,p,-rhoexact,rstride=2, cstride=1, cmap=cm.jet)
+#ax.plot_surface(x,p,rho,rstride=2, cstride=1, cmap=cm.jet)
+ax.plot_surface(x,p,rho-rhoexact,rstride=2, cstride=1, cmap=cm.jet)
+#ax.plot_surface(x,p,-rhoexact,rstride=2, cstride=1, cmap=cm.jet)
 
 ax.set_xlabel('x')
 ax.set_ylabel('p')